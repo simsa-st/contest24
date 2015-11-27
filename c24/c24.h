1614b95f9eee17d015b01a3e013406cedbb753cb
Stepan Simsa <simsa.st@gmail.com>
2015-11-27 17:53:21 +0100

    Add forgotten file c24/c24.h to fix includes.

---
+++ c24/c24.h
@@ -0,0 +1,8 @@
+#include "communication/server_stream.h"
+#include "communication/stream.h"
+#include "communication/stream_backend_interface.h"
+#include "communication/stream_tcp.h"
+#include "communication/stream_tcp_client.h"
+#include "communication/stream_tcp_server.h"
+#include "toolbar/print_variables.h"
+#include "toolbar/sfgui.h"
