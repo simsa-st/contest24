1afe28564f7976891bf2610064daf33a14d180c2
Stepan Simsa <simsa.st@gmail.com>
2016-02-28 02:01:45 +0100

    Candy Eaters, corrections in GameStream.

--- examples/candy_eaters/game_stream.cpp
+++ examples/candy_eaters/game_stream.cpp
@@ -1,45 +1,37 @@
 #include "game_stream.h"
 
-#include <memory>
-
 GameStream::GameStream(std::unique_ptr<StreamBackendInterface> stream_backend)
     : stream_(std::move(stream_backend)) {}
 
 bool GameStream::Wait() {
-  stream_.SendMessageWithCheck("WAIT");
-  if (!stream_.LastStatus().Ok()) return false;
+  if (!stream_.SendMessageWithCheck("WAIT")) return false;
   if (!stream_.GetMessageWithCheck("WAITING")) return false;
-  if (!stream_.LastStatus().Ok()) return false;
   return stream_.GetMessageWithCheck();
 }
 
 bool GameStream::GetInit(int *n) {
-  stream_.SendMessageWithCheck("GET_INIT");
-  if (!stream_.LastStatus().Ok()) return false;
+  if (!stream_.SendMessageWithCheck("GET_INIT")) return false;
   *n = stream_.GetVectorOf<int>(1)[0];
-  return true;
+  return LastStatus().Ok();
 }
 
 bool GameStream::GetMyPos(Pos* pos) {
-  stream_.SendMessageWithCheck("GET_MY_POS");
-  if (!stream_.LastStatus().Ok()) return false;
+  if (!stream_.SendMessageWithCheck("GET_MY_POS")) return false;
   vector<int> raw_pos = stream_.GetVectorOf<int>(2);
   *pos = Pos(raw_pos[0], raw_pos[1]);
-  return true;
+  return LastStatus().Ok();
 }
 
 bool GameStream::GetMyScore(int* score) {
-  stream_.SendMessageWithCheck("GET_MY_SCORE");
-  if (!stream_.LastStatus().Ok()) return false;
+  if (!stream_.SendMessageWithCheck("GET_MY_SCORE")) return false;
   *score = stream_.GetVectorOf<int>(1)[0];
-  return true;
+  return LastStatus().Ok();
 }
 
 bool GameStream::GetCandyCount(int *candy_count) {
-  stream_.SendMessageWithCheck("GET_CANDY_COUNT");
-  if (!stream_.LastStatus().Ok()) return false;
+  if (!stream_.SendMessageWithCheck("GET_CANDY_COUNT")) return false;
   *candy_count = stream_.GetVectorOf<int>(1)[0];
-  return true;
+  return LastStatus().Ok();
 }
 
 bool GameStream::EatCandy() {
